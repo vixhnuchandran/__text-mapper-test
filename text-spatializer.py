c972dcfbc45bd0af6af8ebddf50d99342a67b6a4
vixhnuchandran <vixhnuchandran@gmail.com>
2024-02-29 01:48:46 +0530

    feat: Add image processing using Pillow and Tesseract
    - Implement image processing using Pillow to open and process images.
    - Utilize Tesseract for text extraction and coordinates.
    - Introduce argparse for handling input image path and output file.
    - Enhance functionality for extracting text and coordinates from images.

--- text-spatializer.py
+++ text-spatializer.py
@@ -1,3 +1,17 @@
 import os
 from PIL import Image
 import pytesseract
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+image_path = os.path.join(script_dir, '..', 'images', 'sample.jpg')
+
+image = Image.open(image_path)
+
+text = pytesseract.image_to_osd(image)
+boxes = pytesseract.image_to_boxes(image)
+
+print('Recognized Text: ', text)
+print('Bounding Boxes: ')
+for box in boxes.splitlines():
+    box_info =  box.split()
+    print(f"Word: {box_info[0]}, Coordinates: {box_info[1:5]}")
